0f4af0b9d0464fca63c51bc68e00e25300ac734f
L3D <l3d@see-base.de>
2017-11-06 16:48:55 +0100

    A little update: Luna is there at night

--- ponybot.pl
+++ ponybot.pl
@@ -21,18 +21,20 @@ $VERSION = "1.0";
     contact         => 'l3d@see-base.de',
     name            => "Ponybot",
     description     => "A pony-irssi bot to draw colored ascii ponys in some channels.",
-    version         => "0.2",
+    version         => "0.3",
     status			=> "alpha",
     license         => "GPL-3.0"
 );
 # important global Variables:
-our $main_channel = "ponyville|C6"; #    Use '|' for multiple channels
-our $other_channels = "ccczh|spam"; # Use '|' for multiple channels
+our $main_channel = "ponyville"; #    Use '|' for multiple channels
 our $keyword = "pony|cyber";
 
 # programm
 our $user = getpwuid( $< );
 our @pony;
+our $tmp;
+our $lastpony = `/bin/cat /tmp/lastpony`;
+our @tmp_2;
 Irssi::signal_add 'message public', 'sig_message_public';
 
 sub sig_message_public {
@@ -42,34 +44,11 @@ sub sig_message_public {
         if ($msg =~ m/!(?:$keyword)/i){ # listening for "!$keyword"
             $server->command("msg $target Hey $nick, du hast dir ein Pony gewünscht:");
             get_pony(); # execute 'get_pony' and use a new pony
-            get_pony() if (@pony eq "");
+            # print @pony; # uncomment for advanced debugging
+            print "[I] Debug: $tmp"; 
+            # for my $s (@tmp_2){ print "$s\n"; } # uncomment for advanced debugging
             for my $mlp (@pony){
                 $server->command("msg $target $mlp");
-            }
-        }
-    }
-    elsif ($target =~ m/#(?:$other_channels)/) { # now only operate in these Channels...
-        # Listening for a keyword 2 do sth.
-        if ($msg =~ m/!(?:$keyword)/i){ # listening for "!$keyword"
-            $server->command("msg $target Hey $nick, du hast dir ein Pony gewünscht:");
-            # 2DO: make a few little ponys for big irc channels...
-            get_pony(); # execute 'get_pony' and use a new pony
-            for my $mlp (@pony){
-                $server->command("msg $target $mlp");
-            }
-            $server->command("msg $nick Hey $nick, du hast dir vorhin ein Pony im Channel $target gewünscht.");
-            $server->command("msg $nick Das ist voll okay und kommt auch meistens echt gut an");
-            $server->command("msg $nick ... ");
-            $server->command("msg $nick ABER man muss es ja nicht übertreiben!");
-            $server->command("msg $nick Wenn du regelmäßig Ponys haben möchtest, dann besuche doch mal");
-            $server->command("msg $nick den Kanal #ponyville auch hackint.org oder chaostreff.ch");
-            $server->command("msg $nick :D");
-            $server->command("msg $nick Wenn dir der Bot gefällt oder du funktionen vermisst:");
-            $server->command("msg $nick Schau doch mal auf https://github.com/L3Dokt0r/ponybot vorbei");
-            $server->command("msg $nick Und hier noch ein Pony:");
-            get_pony();
-            for my $mlp (@pony){
-                $server->command("msg $nick $mlp");
             }
         }
     }
@@ -79,11 +58,28 @@ sub sig_message_public {
 sub get_pony {
     my $dirname = "/home/$user/.irssi/ASCII-Pony/rendered/irc/"; # define the directory with the ascii ponys
     my @ascii;
+    my $ponyfile;
     opendir(DIR,"$dirname") || die $!; # How many ponys could we use?
         @ascii = readdir(DIR);
     close DIR;
-    open (DATEI, "/home/$user/.irssi/ASCII-Pony/rendered/irc/$ascii[int(rand($#ascii))-1]") or die $!; # Choose a 'random' pony
+    @ascii = grep {$_ ne '.'} @ascii;
+    @ascii = grep {$_ ne '..'} @ascii;
+    my $hour = (localtime)[2];
+    my $thispony;
+    if ( $hour >= 22 or $hour < 5 ) {
+        $ponyfile = "luna.irc";
+    } else {
+      $thispony = int(rand(int($#ascii) ));
+      while ($thispony eq $lastpony){  $thispony = int(rand(int($#ascii) )); }
+      $ponyfile = $ascii[ $thispony ];
+    }
+    @tmp_2 = @ascii;
+    $tmp = "Ponyanzahl: ";
+    $tmp .= int($#ascii)-1;
+    $tmp .= " Thispony: $thispony";
+    open (DATEI, "/home/$user/.irssi/ASCII-Pony/rendered/irc/$ponyfile") or die $!; # Choose a 'random' pony
     @pony = <DATEI>; # Save pony as global array
     close (DATEI);   
+    `/bin/echo "$thispony" > /tmp/lastpony`
 }
 
